310802c69b626f7841cb319887554f791fb89cce
Maiara Santos <maiarasantos34@yahoo.com>
2022-09-06 14:47:31 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-  return(
-    <h1>olá mundo</h1>
+  return (
+    <>
+      <h1>olá mundo</h1>
+      <NotificationButton />
+      <NotificationButton />
+ 
+    </>
   )
 
 }
