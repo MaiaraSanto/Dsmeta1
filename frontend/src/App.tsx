80ada9c9f9a5d2ec4323c5e83ae13bf4d7eba963
Maiara Santos <maiarasantos34@yahoo.com>
2022-09-09 17:25:45 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
 
@@ -7,6 +9,7 @@ function App() {
   return (
     <>
 
+     <ToastContainer />
       <Header />
        
         <main>
